b2e4377c6f59a06f2b14ccfde6d9290399dbcaf4
molliean <molliean29@gmail.com>
2024-05-07 16:31:46 -0700

    completed lab with level up exercises included

--- script.js
+++ script.js
@@ -173,3 +173,119 @@ function calculateTip(total, pct){
 
 
 console.log('Exercise 9 Result:', calculateTip(50, 20));
+
+
+/*
+Exercise 10: convertTemperature()
+
+Write a function named convertTemperature. It takes two arguments: a temperature and a string representing the scale ('C' for Celsius, 'F' for Fahrenheit). Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+function convertTemperature(temp, scale){
+    let convertCelcius = temp * 9/5 + 32;
+    let convertFahrenheit = (temp - 32) * 5/9;
+    if(scale === 'C'){
+        return `${convertCelcius} (Fahrenheit)`;
+    } else if (scale === 'F'){
+        return `${convertFahrenheit} (Celcius)`;
+    }
+}
+
+
+console.log('Exercise 10 Result:', convertTemperature(32, "F"));
+
+
+/*
+Exercise 11: basicCalculator()
+
+Create a function named basicCalculator. It should take three arguments: two numbers and a string representing an operation ('add', 'subtract', 'multiply', 'divide'). Perform the provided operation on the two numbers. In operations where the order of numbers is important, treat the first parameter as the first operand and the second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+function basicCalculator(num1, num2, str){
+    if(str === 'add'){
+        return num1 + num2;
+    } else if (str === 'subtract'){
+        return num1 - num2;
+    } else if (str === 'multiply'){
+        return num1 * num2;
+    } else if (str === 'divide'){
+        return num1 / num2;
+    }
+}
+
+console.log('Exercise 11 Result:', basicCalculator(10, 5, "subtract"));
+
+
+/*
+Exercise 12: calculateGrade()
+
+Define a function called calculateGrade. It should take a numerical score and return the corresponding letter grade (A, B, C, D, F). For example, 90 and above yields an 'A', 80-89 is a 'B', and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+function calculateGrade(score){
+    if(score >= 90){
+        return 'A';
+    } else if(score >= 80 && score <= 89){
+        return 'B';
+    } else if (score >= 70 && score <= 79){
+        return 'C';
+    } else if (score >= 60 && score <= 69){
+        return 'D';
+    } else if (score < 60){
+        return 'F';
+    }
+}
+
+
+console.log('Exercise 12 Result:', calculateGrade(100));
+
+
+/*
+Exercise 13: createUsername()
+
+Define a function called createUsername. It should take a first name and a last name and return a username. The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre12'.
+
+Complete the exercise in the space below:
+*/
+function createUsername(firstName, lastName){
+    let splitFirst = firstName.split('');
+    let firstPart = `${splitFirst[0]}${splitFirst[1]}${splitFirst[2]}`;
+    let splitLast = lastName.split('');
+    let secondPart = `${splitLast[0]}${splitLast[1]}${splitLast[2]}`;
+    let count = firstName.length + lastName.length;
+    return `${firstPart}${secondPart}${count}`;
+}
+
+
+console.log('Exercise 13 Result:', createUsername("Samantha", "Green"));
+
+
+/*
+Exercise 14: numArgs()
+
+Challenge yourself with numArgs. This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+function numArgs(a, b, c){
+    return arguments.length;
+}
+
+
+console.log('Exercise 14 Result:', numArgs(1, 2, 3, 4, 5));
+
